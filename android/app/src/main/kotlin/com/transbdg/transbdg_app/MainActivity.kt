6ad7bab79f9de142352ae6e771b490f3c6763773
Elvin Ariestha <esthaaaaaaa7@gmail.com>
2026-05-23 10:12:57 +0700

    hapus semua isi repo

--- android/app/src/main/kotlin/com/transbdg/transbdg_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.transbdg.transbdg_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:transbdg_app/main.dart';
-
-void main() {
-  testWidgets('App load test', (WidgetTester tester) async {
-    await tester.pumpWidget(const TransBdgApp());
-    expect(find.byType(TransBdgApp), findsOneWidget);
-  });
-}
